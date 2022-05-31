8c8a60378f4230f39bf96f1cede93eedd4d109f8
sebasY0725 <shebas0725@gmail.com>
2022-05-31 16:54:06 -0500

    Creacion del folder del sidebar mas estilos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Sidebar from "./componentes/sidebar/Sidebar";
 import Topbar from "./componentes/topbar/Topbar";
+import "./app.css";
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+       <div className="others">otros...</div>
+      </div>
     </div>
   );
 }
