6743c1836222c8b9381c298ed7175de95779a0b3
sebasY0725 <shebas0725@gmail.com>
2022-06-01 01:44:49 -0500

    Ultimos cambios, no esta completo

--- src/componentes/Info/Info.jsx
+++ src/componentes/Info/Info.jsx
@@ -1,20 +1,33 @@
-import "./info.css"
+import "./info.css";
 
 export default function Info() {
   return (
-    <div className="info">
+    <div>
+      <div className="info">
         <div className="lastInfo">
-            <span className="NameTitle"> aqui va su name </span>
-            <div className="NumberContainer">
-                <span className="Number">aqui va su sueldo</span>
-            </div>
+          <span className="NameTitle"> Total de usuarios </span>
+          <div className="NumberContainer">
+            <span className="Number">aqui va el numero</span>
+          </div>
         </div>
         <div className="lastInfo">
-            <span className="NameTitle"> aqui va su name de area </span>
-            <div className="NumberContainer">
-                <span className="Number">aqui va su codigo</span>
-            </div>
+          <span className="NameTitle"> Total de areas </span>
+          <div className="NumberContainer">
+            <span className="Number">aqui va el numero</span>
+          </div>
         </div>
+        <div className="lastInfo">
+          <span className="NameTitle"> Cantidad de usuario por area </span>
+          <div className="NumberContainer">
+            <span className="Number">aqui va el numero</span>
+          </div>
+        </div>
+      </div>
+      <ul>
+          <li>
+              Lider 1 
+          </li>
+      </ul>
     </div>
-  )
+  );
 }
--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -1,50 +1,77 @@
-import "./newuser.css"
+import "./newuser.css";
+import { useState, useEffect } from "react";
 
-export default function newUser() {
+export default function NewUser() {
+  const [{nombre,apellido,email,cc,area,salario}, setData] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    cc: "",
+    area: "",
+    salario: "",
+  });
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    setData({
+      nombre: e.target,
+      apellido: e.target,
+      email: e.target,
+      cc: e.target,
+      area: e.target,
+      salario: e.target
+    });
+
+    console.log(nombre.nombre);
+  };
   return (
     <div className="newUser">
-        <h1 className="newUSerTittle">Nuevo usuario</h1>
-        <form className="newForm">
-            <div className="newItem">
-                <label>Nombres</label>
-                <input type="text" placeholder="Nombres" />
-            </div>
-             <div className="newItem">
-                <label>Apellidos</label>
-                <input type="text" placeholder="Apellidos" />
-            </div>
-             <div className="newItem">
-                <label>Fecha de nacimiento</label>
-                <input type="date" />
-            </div>
-             <div className="newItem">
-                <label>Email</label>
-                <input type="email" placeholder="email" />
-            </div>
-            <div className="newItem">
-                <label>Numero de docuemento</label>
-                <input type="number" placeholder="CC" />
-            </div>
-               <div className="newItem">
-                <label>Area </label>
-                <input type="number" placeholder="Area" />
-            </div>
-               <div className="newItem">
-                <label>Salario</label>
-                <input type="number" placeholder="salario" />
-            </div>
-            <div className="newItem">
-                <label>Estado</label>
-                <select className="Estate" name="active" id="active">
-                    <option value="Activo">Activo</option>
-                    <option value="Inactivo">Inactivo</option>
-                </select>
-        
-            </div>
-            <button className="newButton">Crear</button>
-           
-            
-        </form>
+      <h1 className="newUSerTittle">Nuevo usuario</h1>
+      <form 
+            onSubmit={handleSubmitForm}
+            className="newForm"
+          >
+  
+        <div className="newItem">
+          <label>Nombres</label>
+          <input type="text" placeholder="Nombres" value={nombre} />
+        </div>
+        <div className="newItem">
+          <label>Apellidos</label>
+          <input type="text" placeholder="Apellidos" value={apellido} />
+        </div>
+        <div className="newItem">
+          <label>Fecha de nacimiento</label>
+          <input type="date" />
+        </div>
+        <div className="newItem">
+          <label>Email</label>
+          <input type="email" placeholder="email" value={email} />
+        </div>
+        <div className="newItem">
+          <label>Numero de docuemento</label>
+          <input type="number" placeholder="CC" value={cc} />
+        </div>
+        <div className="newItem">
+          <label>Area </label>
+          <input type="number" placeholder="Area" value={area} />
+        </div>
+        <div className="newItem">
+          <label>Salario</label>
+          <input type="number" placeholder="salario" value={salario} />
+        </div>
+        <div className="newItem">
+          <label>Estado</label>
+          <select className="Estate" name="active" id="active">
+            <option value="Activo">Activo</option>
+            <option value="Inactivo">Inactivo</option>
+          </select>
+        </div>
+        <button className="newButton" type="submit" value="Crear">
+          Crear
+        </button>
+      </form>
     </div>
-  )
+  );
 }
